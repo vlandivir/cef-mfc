34a36d9d40eda13dd8cd463e09722a20faa6b36d
Vladimir Rybakov <vladimir.rybakov@gmail.com>
2012-03-21 17:01:11 +0400

    change url test

--- ClientHandler.cpp
+++ ClientHandler.cpp
@@ -2,6 +2,8 @@
 #include "ClientHandler.h"
 #include "CEFView.h"
 
+#include <sstream>
+
 ClientHandler::ClientHandler(void)
 {
 }
@@ -35,6 +37,79 @@ bool ClientHandler::OnBeforeResourceLoad(CefRefPtr<CefBrowser> browser,
                                          int loadFlags)
 {
   CefString url = request->GetURL();
+  CefURLParts parts;
+  CefParseURL(url, parts);
+  if(CefString(&parts.host) == "server")
+  {
+      class RequestClient : public CefWebURLRequestClient
+      {
+      public:
+        RequestClient(CefRefPtr<CefBrowser> browser) : browser_(browser) {}
+
+        virtual void OnStateChange(CefRefPtr<CefWebURLRequest> requester, 
+                                   RequestState state)
+        {
+          REQUIRE_UI_THREAD();      
+          if (state == WUR_STATE_DONE) 
+          {
+            //buffer_ = StringReplace(buffer_, "<", "&lt;");
+            //buffer_ = StringReplace(buffer_, ">", "&gt;");
+            std::stringstream ss;
+            ss << "<html><body>Source:<pre>" << buffer_ << "</pre></body></html>";
+
+            browser_->GetMainFrame()->LoadString(ss.str(),
+                "http://tests/weburlrequest");
+          }
+          
+        }
+    
+        virtual void OnRedirect(CefRefPtr<CefWebURLRequest> requester, 
+                                CefRefPtr<CefRequest> request, 
+                                CefRefPtr<CefResponse> response)
+        {
+          REQUIRE_UI_THREAD();
+        }
+    
+        virtual void OnHeadersReceived(CefRefPtr<CefWebURLRequest> requester, 
+                                       CefRefPtr<CefResponse> response)
+        {
+          REQUIRE_UI_THREAD();
+        }
+    
+        virtual void OnProgress(CefRefPtr<CefWebURLRequest> requester, 
+                                uint64 bytesSent, uint64 totalBytesToBeSent)
+        {
+          REQUIRE_UI_THREAD();
+        }
+    
+        virtual void OnData(CefRefPtr<CefWebURLRequest> requester, 
+                            const void* data, int dataLength)
+        {
+          REQUIRE_UI_THREAD();
+          buffer_.append(static_cast<const char*>(data), dataLength);
+        }
+    
+        virtual void OnError(CefRefPtr<CefWebURLRequest> requester, 
+                             ErrorCode errorCode)
+        {
+          REQUIRE_UI_THREAD();
+        }
+
+      protected:
+        CefRefPtr<CefBrowser> browser_;
+        std::string buffer_;
+
+        IMPLEMENT_REFCOUNTING(CefWebURLRequestClient);
+      };
+
+      CefRefPtr<CefRequest> request(CefRequest::CreateRequest());
+      request->SetURL(url);
+
+      CefRefPtr<CefWebURLRequestClient> client(new RequestClient(browser));
+      CefRefPtr<CefWebURLRequest> requester(
+          CefWebURLRequest::CreateWebURLRequest(request, client));
+      return true;
+  }
   return false;
 }
 
